11792f1334957223e3e3fa4a302d261abe7fa6f1
meali029 <mehboobaliali150@gmail.com>
2025-11-11 04:54:42 +0500

    fix:production bug fixed CLIENt_URL

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 
 // CORS middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true
 }));
 
